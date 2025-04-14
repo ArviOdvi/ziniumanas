d32e2db27064bc2bb4ff25a83ad172c6b4b42564
Arvydas <arvydo.viducio@gmail.com>
2025-04-14 19:16:18 +0300

    Initial App

---
+++ src/main/java/lt/ziniumanas/ZiniumanasApp.java
@@ -0,0 +1,13 @@
+package lt.ziniumanas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZiniumanasApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZiniumanasApp.class, args);
+    }
+
+}
