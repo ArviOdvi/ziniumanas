e93baf34f9cf6d3c8442d3ad95f8c7e5678355aa
Arvydas <arvydo.viducio@gmail.com>
2025-07-15 20:59:49 +0300

    Sukurta klasė JwtSecretGenerator.java tokenų generacijai.

---
+++ src/main/java/lt/ziniumanas/config/JwtSecretGenerator.java
@@ -0,0 +1,28 @@
+package lt.ziniumanas.config;
+
+import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
+import java.security.SecureRandom;
+import java.util.Base64;
+
+@Getter
+@Component
+public class JwtSecretGenerator {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtSecretGenerator.class);
+    private String jwtSecret;
+
+    @PostConstruct
+    public void init() {
+        SecureRandom random = new SecureRandom();
+        byte[] key = new byte[32]; // 256 bitai
+        random.nextBytes(key);
+        jwtSecret = Base64.getEncoder().encodeToString(key);
+        logger.info("Generated JWT secret: {}", jwtSecret);
+    }
+
+}
