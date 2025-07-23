a4b6c2455fd26f91d3b6f223982236e585524b89
Arvydas <arvydo.viducio@gmail.com>
2025-07-23 10:58:44 +0300

    Sukurta atskiros klasės SpaRedirectConfig.java ir StaticResourceConfig.java vietoj WebConfig.java klasės.

--- src/main/java/lt/ziniumanas/config/SpaRedirectConfig.java
+++ src/main/java/lt/ziniumanas/config/SpaRedirectConfig.java
@@ -1,29 +1,11 @@
 package lt.ziniumanas.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNull;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/")
-                .resourceChain(true);
-    }
-
-    @Override
-    public void addCorsMappings(@NonNull CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("*");
-    }
-
+public class SpaRedirectConfig implements WebMvcConfigurer{
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/{spring:[\\w\\-]+}")
